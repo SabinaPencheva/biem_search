3a7df5f76f43afe47401eca18de40eb4ec65b033
BuildTools <unconfigured@null.spigotmc.org>
2016-12-12 16:40:20 +0100

    "Added sensitivity evaluator"

---
+++ evaluator/sensitivity.py
@@ -0,0 +1,21 @@
+from .base import AbstractEvaluator
+
+
+class Sensitivity(AbstractEvaluator):
+    def name(self):
+        return "Sensitivity"
+
+    def __init__(self, data, cutoff):
+        self.total_matches = 0
+        self.cutoff = cutoff
+        for entry in data:
+            if entry[-1] == '1':
+                self.total_matches += 1
+
+    def evaluate(self, subgroup):
+        matches_in_group = 0
+        for index in subgroup[1]:
+            if self.is_match(index):
+                matches_in_group += 1
+        #Sensitivity quality evaluator: p/P
+        return matches_in_group / self.total_matches
